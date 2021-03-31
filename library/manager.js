43c886ee129e40d8ac132e6ebb5dc0e5e6748a40
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-03-31 13:55:30 -0400

    Update manager.js

---
+++ library/manager.js
@@ -0,0 +1,17 @@
+const Employee = require("./employee.js");
+
+class Manager extends Employee{
+    constructor(name, id, email, officeNumber, position) {
+        super(name, id, email):
+        this.officeNumber = officeNumber
+        this.role = role
+    }
+    getOfficeNumber() {
+        return this.officeNumber
+    }
+    getPosition() {
+        return "Manager";
+    };
+};
+
+module.exports = Manager
