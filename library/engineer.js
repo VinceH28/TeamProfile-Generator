8ae558b3b9f78ab78a3d7c37f924a2ed9a2b8333
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-03-31 13:45:41 -0400

    Update engineer.js

---
+++ library/engineer.js
@@ -0,0 +1,22 @@
+class Employee {
+    constructor(name, id, email, role) {
+        this.name = name;
+        this.id = id;
+        this.email = email
+        this.position = this.position
+    }
+    getName() {
+        return this.name
+    }
+    getID() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getPosition(){
+        return "Employee"
+    }
+}
+
+module.exports = Employee
