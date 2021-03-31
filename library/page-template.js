f5f10c84c81ef7dea7577534c17276d1978c3ab8
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-03-31 16:33:45 -0400

    Update page-template.js

--- library/page-template.js
+++ library/page-template.js
@@ -127,3 +127,44 @@ const createEmployee = (Employee) => {
     `;
 }
 
+module.exports = function generateTeam(team) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+          crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="style.css" />
+        <title>My Team</title>
+      </head>
+      <body>
+        <div id="jumbo" class="jumbotron jumbotron-fluid text-white">
+          <div class="container">
+            <h1 class="display-3 text-center">My Team</h1>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+                <div class="team-area col-12 d-flex
+                justify-content-center">
+                    ${createTeam(team)}
+                </div>
+            </div>
+        </div>
+      </body>
+    </html>
+    
+    `;
+};
