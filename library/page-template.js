8ae8fcb7e9b101fafa513edc3b6fd60ebb755c77
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-03-31 16:33:16 -0400

    Update page-template.js

--- library/page-template.js
+++ library/page-template.js
@@ -84,3 +84,26 @@ const createEngineer = (engineer) => {
     `;
 }
 
+// create intern html
+const createIntern = (intern) => {
+    return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${intern.getPosition()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: 
+                ${intern.getID()}
+                </li>
+                <li class="list-group-item">
+                Email: <a href="mailto: ${intern.getEmail}"> ${intern.getEmail}</a>
+                </li>
+                <li class="list-group-item"> Institution: ${intern.getInstitution}</li>
+            </ul>
+        </div>
+    </div> 
+    `;
+}
+
