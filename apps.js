2386e8d1ffe8329145c606f60dc289241fbd9d30
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-03-31 23:59:24 -0400

    Update apps.js

--- apps.js
+++ apps.js
@@ -22,3 +22,64 @@ function phoneNumber(phone) {
     return phoneno.test(phone);
 }
 
+//intialize inquirer.prompt npm to ask questions and parse the input/validate answers
+async function init() {
+    const managerData = await inquirer.prompt ([
+        {
+                type: "input",
+                name: "name",
+                message: "What is the team manager's name?",
+                validate: name => {
+                if (name) {
+                    return true
+                } else { 
+                    console.log("Please enter a name")}
+                return true;
+            },
+        },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the team manager's id?",
+                validate: id => {
+                    if (id > 0) {
+                        return true;
+                    } else {
+                        console.log("Please enter a NUMBER greate than zero");}
+                    return false
+
+            }
+        },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the team manager's email?",
+                validate: email => {
+                    if (email && email.includes("@")) {
+                        return true;
+                    } else {
+                      console.log("Please enter a valid email address")}
+                    return false
+            }
+        },
+        
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the team manager's phone number?",
+                validate: async (input) => {
+                    if (!phoneNumber(input)) return "Please enter a valid phone number. ";
+                    return true;}
+        },
+        
+            {
+                type: "input",
+                name: "members",
+                message: "How many team members are there?",
+                validate: async (input) => {
+                    if (Number(input) <= 0) return "Please add a team member";
+                    return true;
+                },
+        },   
+    ]);
+
